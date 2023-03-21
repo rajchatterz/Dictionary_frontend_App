e06b2c5e9d2139b9029d5f8b8a8d4ed4e3c881ff
Sreenidhi Ganachari <creativeme521@gmail.com>
2023-03-21 14:28:22 +0530

    Added changes to login  section

--- store/auth-context.js
+++ store/auth-context.js
@@ -4,18 +4,27 @@ import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext({
   token: '',
+  contact:'',
   isAuthenticated: false,
   authenticate: (token) => {},
+  authenticateContact:(contact)=>{},
   logout: () => {},
 });
 
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState();
+  const [authContact, setContact] = useState();
 
   function authenticate(token) {
     setAuthToken(token);
     AsyncStorage.setItem('token', token);
   }
+
+  function authenticateContact (contact) {
+     setContact(contact);
+      AsyncStorage.setItem('contact', contact);
+ 
+    }
 
   function logout() {
     setAuthToken(null);
@@ -24,8 +33,10 @@ function AuthContextProvider({ children }) {
 
   const value = {
     token: authToken,
+    contact:authContact,
     isAuthenticated: !!authToken,
     authenticate: authenticate,
+    authenticateContact:authenticateContact,
     logout: logout,
   };
 
