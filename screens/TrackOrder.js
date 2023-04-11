ebdfba3123d70394cc83106db3e0b2ff38f19c2f
Sreenidhi Ganachari <creativeme521@gmail.com>
2023-04-11 12:44:05 +0530

    Added Order changes 3.0

--- screens/TrackOrder.js
+++ screens/TrackOrder.js
@@ -7,97 +7,58 @@ export default class TrackOrder extends React.Component {
     const orders = [
       {
         id: 1,
-        name: 'Package from Warrior Royals - Shoes', orderId: '123456', description: 'Size : 10', image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs',
+        companyname: 'Package from Warrior Royals', name:'Shoes',   status: 'Order delivered', active:true,  date: 'Amazon order on Jun 10  , 2020 at 09:30', orderId: '123456', description: 'Size : 10', image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs', price: 19.99,
         data: [
 
-          { title: 'Order Placed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been placed.', subtitletime: "Fri , 5th Jun '20 - 5:03pm" },
-          { title: 'Order Confirmed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been confirmed.', subtitletime: "Fri , 5th Jun '20 - 5:10pm" },
-          { title: 'Order Packed', isCurrent: true, titletime: "Fri , 8th Jul '20", subtext: 'Your order has been packed.', subtitletime: "Mon , 8th Jun '20 - 8:20am" },
-          { title: 'Order Dispatched', isCurrent: true, titletime: "Fri , 9th Jul '20", subtext: 'Your order has been dispatched.', subtitletime: "Tue , 9th Jun '20 - 10:00pm" },
-          { title: 'Order Delivered', isCurrent: true, titletime: "Fri , 10th Jul '20", subtext: 'Your order is yet to be delivered.', subtitletime: "" },
+          { statustitle: 'Order Placed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been placed.', subtitletime: "Fri , 5th Jun '20 - 5:03pm" },
+          { statustitle: 'Order Confirmed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been confirmed.', subtitletime: "Fri , 5th Jun '20 - 5:10pm" },
+          { statustitle: 'Order Packed', isCurrent: true, titletime: "Fri , 8th Jul '20", subtext: 'Your order has been packed.', subtitletime: "Mon , 8th Jun '20 - 8:20am", },
+          { statustitle: 'Order Dispatched', isCurrent: true, titletime: "Fri , 9th Jul '20", subtext: 'Your order has been dispatched.', subtitletime: "Tue , 9th Jun '20 - 10:00pm" },
+          { statustitle: 'Order Delivered', isCurrent: false, titletime: "Fri , 10th Jul '20", subtext: 'Your order is yet to be delivered.', subtitletime: "" },
         ],
       },
 
-
-    ];
-
-    const pastOrders = [
-      {
-        id: 1,
-        date: 'Amazon order on Jan 28 , 2023 at 09:30',
-        status: 'Order delivered',
-        items: [
-          {
-            id: 1,
-            companyname: 'Warriors - Royal',
-            name: 'T-Shirt',
-            price: 19.99,
-            image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs'
-          },
-        ]
-      },
       {
         id: 2,
-        date: 'Amazon order on Jan 28 , 2023 at 09:30',
-        status: 'Order confirmed',
-        items: [
-          {
-            id: 3,
-            name: 'Sweatshirt',
-            companyname: 'Warriors - Royal',
-            price: 29.99,
-            image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs'
-          }
-        ]
+        companyname: 'Package from Warrior Royals', name:'Shoes',   status: 'Order delivered', active:false,   date: 'Amazon order on Jun 10  , 2020 at 09:30', orderId: '123456', description: 'Size : 10', image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs', price: 19.99,
+        data: [
+
+          { statustitle: 'Order Placed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been placed.', subtitletime: "Fri , 5th Jun '20 - 5:03pm" },
+          { statustitle: 'Order Confirmed', isCurrent: true, titletime: "Fri , 5th Jul '20", subtext: 'Your order has been confirmed.', subtitletime: "Fri , 5th Jun '20 - 5:10pm" },
+          { statustitle: 'Order Packed', isCurrent: true, titletime: "Fri , 8th Jul '20", subtext: 'Your order has been packed.', subtitletime: "Mon , 8th Jun '20 - 8:20am" },
+          { statustitle: 'Order Dispatched', isCurrent: true, titletime: "Fri , 9th Jul '20", subtext: 'Your order has been dispatched.', subtitletime: "Tue , 9th Jun '20 - 10:00pm" },
+          { statustitle: 'Order Delivered', isCurrent: true, titletime: "Fri , 10th Jul '20", subtext: 'Your order is yet to be delivered.', subtitletime: "" },
+        ],
       },
-      {
-        id: 3,
-        date: 'Amazon order on Jan 28 , 2023 at 09:30',
-        status: 'Order delivered',
-        items: [
-          {
-            id: 4,
-            name: 'Sneakers',
-            companyname: 'Warriors - Royal',
-            price: 79.99,
-            image: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/uckbx3rr87jhakb81mbs'
-          },
-        ]
-      }
+     
+
+
     ];
+
+    
     return (
-      <ScrollView>
-        <Text style={{
-          fontWeight: 'bold',
-          fontSize: 18,
-        }}>Now</Text>
+      <ScrollView  style={{
+       
+        marginBottom: 40
+      }}>
+        
+       
         <View style={{ flex: 1, borderRadius: 10, marginTop: 10 }}>
-          {orders.map((order) => (
-            <View style={{
-              backgroundColor: 'white',
-              borderRadius: 5,
-              marginBottom: 20,
-              padding: 20,
-            }}>
-              <View style={styles.header}>
-                <Image style={styles.itemImagenow} source={{ uri: order.image }} />
-                <View style={styles.headerTextContainer}>
-                  <Text style={styles.headerTitle}>Order Tracking : {order.orderId}</Text>
-
-                  <Text style={styles.headerText}>{order.name}</Text>
-
-                </View>
-              </View>
-              <MapProgress key={order.id} data={[...order.data]} />
-            </View>
-          ))}
-          <Text style={{
-            fontWeight: 'bold',
-            fontSize: 18,
-          }}>Past</Text>
-          {pastOrders.map((order) => (
-            <View key={order.id} style={styles.orderContainer}>
-              <Text style={styles.dateText}>{order.date}</Text>
+        {orders.map((order) => {
+  if (order.active==false) {
+   
+    return (
+      <><Text style={{
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 10,
+      }}>Past</Text><View style={{
+        backgroundColor: 'white',
+        borderRadius: 5,
+        marginBottom: 20,
+        padding: 15,
+      }} key={order.id}>
+           <Text style={styles.dateText}>{order.date}</Text>
               <View
                 style={{
                   borderBottomColor: 'grey',
@@ -105,25 +66,50 @@ export default class TrackOrder extends React.Component {
                   marginBottom: 10
                 }}
               />
+                       <View style={styles.itemsContainer}>
+              
+              <View key={order.id} style={styles.itemContainer}>
+                <Image style={styles.itemImage} source={{ uri: order.image }} />
+                <View style={styles.itemDetails}>
+                  <Text style={styles.statusText}>{order.status}</Text>
+                  <Text style={styles.itemName}>{order.companyname}</Text>
+                  <Text style={styles.itemName}>{order.name}</Text>
 
-              <View style={styles.itemsContainer}>
-                {order.items.map((item) => (
-                  <View key={item.id} style={styles.itemContainer}>
-                    <Image style={styles.itemImage} source={{ uri: item.image }} />
-                    <View style={styles.itemDetails}>
-                      <Text style={styles.statusText}>{order.status}</Text>
-                      <Text style={styles.itemName}>{item.companyname}</Text>
-                      <Text style={styles.itemName}>{item.name}</Text>
-
-                    </View>
-                  </View>
-                ))}
+                </View>
               </View>
+              </View>
+        </View></>
+    );
+  } else {
+   
+    return (
+      <><Text style={{
+        fontWeight: 'bold',
+        fontSize: 18,
+      }}>Now</Text><View style={{
+        backgroundColor: 'white',
+        borderRadius: 5,
+        marginTop:10,
+        marginBottom: 20,
+        padding: 20,
+      }} key={order.id}>
+
+          <View style={styles.header}>
+            <Image style={styles.itemImagenow} source={{ uri: order.image }} />
+            <View style={styles.headerTextContainer}>
+              <Text style={styles.headerTitle}>Order Tracking : {order.orderId}</Text>
+              <Text style={styles.headerText}>{order.companyname} - {order.name}</Text>
+             
             </View>
-          ))}
+          </View>
+          <MapProgress key={order.id} data={[...order.data]} />
+        </View></>
+    );
+  }
+})}
         </View>
 
-      </ScrollView>
+      </ScrollView >
     );
   }
 }
