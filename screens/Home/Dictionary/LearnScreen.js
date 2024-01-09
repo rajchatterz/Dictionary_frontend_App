164a8cfcdee145d0b8b6e476116a271fe4dc02d3
rajchatterz <iamraj.space@gmail.com>
2024-01-09 19:40:16 +0530

    done with the learn screen

--- screens/Home/Dictionary/LearnScreen.js
+++ screens/Home/Dictionary/LearnScreen.js
@@ -1,28 +1,65 @@
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import React, { useState, useEffect, useContext } from "react";
-// import DummyData from "../../../LakshitModule/DummyData";
-import * as Progress from "react-native-progress";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-import AntDesign from "react-native-vector-icons/AntDesign";
-import { useNavigation } from "@react-navigation/native";
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StyleSheet, Text, View,Image, FlatList } from 'react-native'
+import React,{useContext,useState,useEffect,useCallback} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { AuthContext } from "../../../store/auth-context";
-import Loading from "./Loading";
+import axios from 'axios';
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import * as Progress from "react-native-progress";
+import Loading from './Loading';
+import { useFocusEffect } from '@react-navigation/native';
+const Tab = createMaterialTopTabNavigator()
 
-export default function WordCategory() {
+const Home = ({navigation}) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { token } = useContext(AuthContext)
+  const RenderItem = ({item}) => {
+    return (
+      <View style={styles.flatContainer}>
+        <View style={styles.cardContainer}>
+          <View style={styles.flatListImg}>
+            
+          </View>
+          <View>
+            <View style={styles.sidetopView}>
+            <Text style={{fontSize:18,fontWeight:'500'}}>{item.name}</Text>
+          <Text style={{top:-4, fontSize:12}}>Progress 60%</Text>
+          <Progress.Bar
+                    style={{top:-8}}
+                    progress={item.progress}
+                    color={"#A780E8"}
+                    width={150}
+                    borderWidth={1}
+                    borderColor={"#A780E8"}
+                    unfilledColor={"white"}
+                    height={7}
+                    animationType="timing"
+            borderRadius={10}
 
-  const { token } = useContext(AuthContext);
-  console.log(token);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        // const token = await AsyncStorage.getItem("token");
-
+          />
+          <View style={styles.bottom}>
+            <Text style={{fontSize:11}}>300 Word</Text>
+            <FontAwesome5
+                    onPress={() => navigation.navigate("WordList")}
+                    style={{ textAlign: "right", left:80 }}
+                    name="arrow-circle-right"
+                    size={22}
+                    color={"#8F6ACD"}
+                  />
+          </View>
+          </View>
+          
+        </View>
+          </View>
+      </View>
+    )
+  }
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async() => {
+        try {
+          setLoading(true)
         if (!token) {
           console.error("Token not found in AsyncStorage");
           return;
@@ -34,155 +71,150 @@ export default function WordCategory() {
               Authorization: `Bearer ${token}`,
             },
           }
-        );
-        const newData = response.data.data;
-        setData(newData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
+        )
+        const newData = response.data.data
+        setData(newData)
+        } catch (error) {
+          console.error("error found");
+        } finally {
+          setLoading(false)
+        }
       }
-    };
+      fetchData()
+    },[token])
+  )
 
-    fetchData();
-  }, []);
-  const navigation = useNavigation();
+  
 
   if (!loading) {
     return (
-      <ScrollView horizontal={false} showsVerticalScrollIndicator={false}>
-        <View style={styles.container}>
-          <AntDesign
-            onPress={() => navigation.goBack()}
-            style={{ top: 100, right: 180 }}
-            name="arrowleft"
-            size={22}
-            color={"#FFFFFF"}
-          />
-          <Image
-            style={styles.curve}
-            source={require("../../../assets/Group1.png")}
-          />
-          <Text style={styles.word}>Word Categories</Text>
-          <View style={styles.cardcontainer}>
-            <ScrollView
-              showsVerticalScrollIndicator={false}
-              horizontal={false}
-              contentContainerStyle={{ bottom: 15 }}
-            >
-              {data.map((item) => (
-                <View key={item.name} style={styles.listcard}>
-                  <Image
-                    style={styles.cardimage}
-                    source={{ uri: item.image }}
-                  />
-                  <Text style={styles.cardtext}>{item.name}</Text>
-                  <Text
-                    style={{
-                      textAlign: "left",
-                      left: 115,
-                      bottom: 95,
-                      fontSize: 10,
-                    }}
-                  >
-                    {item.word} {item.progress}
-                  </Text>
-                  <Progress.Bar
-                    style={{ bottom: 90, left: 115 }}
-                    progress={item.progress}
-                    color={"#A780E8"}
-                    width={150}
-                    borderWidth={1}
-                    borderColor={"#A780E8"}
-                    unfilledColor={"white"}
-                    height={7}
-                    animationType="timing"
-                    borderRadius={10}
-                  />
-                  <Text
-                    style={{
-                      textAlign: "left",
-                      left: 115,
-                      bottom: 75,
-                      fontSize: 13,
-                    }}
-                  >
-                    {item.totalWords}
-                  </Text>
-                  <FontAwesome5
-                    onPress={() => navigation.navigate("WordList")}
-                    style={{ textAlign: "right", bottom: 93, right: 15 }}
-                    name="arrow-circle-right"
-                    size={22}
-                    color={"#8F6ACD"}
-                  />
-                </View>
-              ))}
-            </ScrollView>
-          </View>
-        </View>
-      </ScrollView>
-    );
+      <View style={styles.middleContainer}>
+        <FlatList
+        data={data}
+        renderItem={RenderItem}
+        
+      />
+      </View>
+    )
   } else {
-    return <Loading />;
+    return <Loading/>
   }
 }
+const Setting = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+const LearnScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={{ fontSize:26, fontWeight: 'bold',color:'white',left:20,top:10}}>Word Categories</Text>
+        <Image style={styles.imgView} source={require('../../../assets/Group1.png')} />
+      </View>
+      <View style={styles.bottomContainer}>
+        <Tab.Navigator>
+          <Tab.Screen options={{
+            
+            tabBarLabelStyle: { fontSize: 22, fontWeight:'700',textTransform:'none',color:'#6829ce' },
+            tabBarIndicatorStyle: {
+              height: 3,
+              width: 69,
+              left: 19,
+              bottom:6,
+              backgroundColor: '#A780E8',
+              
+            },
+            tabBarStyle:{width:220}
+          }} name='General' component={Home} />
+          <Tab.Screen
+            options={{
+              
+              tabBarLabelStyle: { fontSize: 22,textTransform:'none', color:'#6829ce',fontWeight:'700'  },
+              tabBarIndicatorStyle: {
+                height: 3,
+                bottom:6,
+                width: 66,
+                left: 22,
+                backgroundColor:'#A780E8'
+              },
+              tabBarStyle:{width:220}
+            }}
+            name='Exams' component={Setting} />
+          </Tab.Navigator>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default LearnScreen
 
 const styles = StyleSheet.create({
+  topContainer: {
+    flex: 1,
+    backgroundColor:'#A780E8'
+  },
+  bottomContainer: {
+    flex: 3,
+    backgroundColor: 'white',
+    flexDirection:'row'
+  },
+  imgView: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'cover',
+    top: 20,
+    
+    
+  },
+  flatContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  middleContainer: {
+    flex:1
+  },
   container: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#A678F2",
+    flex:1
   },
-  curve: {
-    width: "100%",
-    height: "25%",
-    top: 120,
-  },
-  cardcontainer: {
-    width: "100%",
-    height: "80%",
-    bottom: 60,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
-    borderTopLeftRadius: 32,
-    borderTopRightRadius: 32,
-  },
-  word: {
-    fontSize: 35,
-    right: 50,
-    bottom: 80,
-    fontWeight: "900",
-    color: "white",
-    lineHeight: 34.48,
-    letterSpacing: -0.28,
-    marginTop: 150
-  },
-  listcard: {
-    backgroundColor: "white",
-    width: 360,
+  flatListImg: {
+    width: 100,
     height: 100,
+    borderWidth: 1,
+    borderColor: 'black',
+  
+  },
+  sidetopView: {
+    justifyContent: 'space-between',
+    flex: 1,
+    left:15
+  },
+  flatListheading: {},
+  bottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between'
+  },
+  cardContainer: {
+    backgroundColor: 'white',
+    width: '90%',
+    margin: 10,
+    padding: 10,
     borderRadius: 10,
-    marginBottom: 15,
-    top: 40,
-    elevation: 4,
-    shadowColor: "#000000",
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.23,
-    shadowRadius: 10,
-  },
-  cardimage: {
-    width: 102,
-    height: 100,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-  },
-  cardtext: {
-    textAlign: "left",
-    bottom: 100,
-    fontSize: 15,
-    fontWeight: "900",
-    left: 115,
-  },
-});
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    flexDirection:'row'
+  }
+  
+})
